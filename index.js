4e6018c8909dd8a14d811e10be1036d35c8e358a
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-10-01 16:48:48 +0600

    root api created

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const app = express()
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/', (req, res) =>{
+  res.send(`Hello from DB it's working!`);
+})
 
 const client = new MongoClient(uri, { useNewUrlParser: true,  useUnifiedTopology: true  });
 client.connect(err => {
